a26f1d1664315e6a0d11bbf0b6db53909bf799dd
Grace Feng <v-linfen@microsoft.com>
2017-09-04 13:55:37 +0800

    Tcp between wpf server and xamarin android client.

---
+++ TCPClient/TCPClient/MainActivity.cs
@@ -0,0 +1,44 @@
+﻿using Android.App;
+using Android.Widget;
+using Android.OS;
+using System.Net.Sockets;
+using System.Net;
+using System;
+
+namespace TCPClient
+{
+    [Activity(Label = "TCPClient", MainLauncher = true, Icon = "@drawable/icon")]
+    public class MainActivity : Activity
+    {
+        private TcpClient tcpclient;
+
+        protected override void OnCreate(Bundle bundle)
+        {
+            base.OnCreate(bundle);
+
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            Button btn = FindViewById<Button>(Resource.Id.btn);
+            btn.Click += (sender, e) =>
+           {
+               Toast.MakeText(this, "Connection waiting", ToastLength.Long);
+               try
+               {
+                   IPAddress ip = IPAddress.Parse("10.106.93.197"); //Ip from textbox
+                   tcpclient = new TcpClient();
+                   tcpclient.ConnectAsync(ip, 8126);
+
+                   if (tcpclient.Connected)
+                   {
+                       Toast.MakeText(this, "Connected Successfully!", ToastLength.Long);
+                   }
+               }
+               catch (System.Exception ex)
+               {
+                   Toast.MakeText(this, ex.Message, ToastLength.Long);
+               }
+           };
+        }
+    }
+}
---
+++ WPFServer/WPFServer/MainWindow.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Windows.Threading;
+
+namespace WPFServer
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private TcpListener listner = new TcpListener(IPAddress.Any, 8126);
+        private NetworkStream netstream;
+        private TcpClient client;
+        private byte[] datalength = new byte[4];
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            Thread startconnection = new Thread(new ThreadStart(connect));
+            startconnection.Start();
+        }
+
+        public void connect()
+        {
+            try
+            {
+                listner.Start(); //Starts Listening to any IP with port 8126
+                //Invoked Just because of CrossThreading Error
+
+                this.lbl_status.Dispatcher.Invoke(DispatcherPriority.Normal,
+                    new Action(() => { this.lbl_status.Text = "Status : Waiting for Client"; }));
+
+                //Invoke Complete.
+
+                //listner.Server.Listen(1);
+                client = listner.AcceptTcpClient();
+
+                if (client.Connected)
+                {
+                    this.lbl_status.Dispatcher.Invoke(DispatcherPriority.Normal,
+                       new Action(() => { this.lbl_status.Text = "Status : Connected Successfully"; }));
+
+                    //serverReceive();
+                }
+            }
+            catch (Exception ex)
+            {
+                //lbl_status.Text = "Status : " + ex.Message;
+                MessageBox.Show(ex.Message);
+            }
+        }
+    }
+}
